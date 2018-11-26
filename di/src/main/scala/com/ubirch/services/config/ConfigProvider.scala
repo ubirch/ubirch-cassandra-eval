10708843d853b93af2c545a2103508f9441fdb77
Carlos Augusto Sánchez <caaugustoss@gmail.com>
2018-11-26 08:31:36 -0500

    UP-357 Cassandra Cluster - Quill Context Management #comment Removing unused code.

--- di/src/main/scala/com/ubirch/services/config/ConfigProvider.scala
+++ di/src/main/scala/com/ubirch/services/config/ConfigProvider.scala
@@ -4,8 +4,6 @@ import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import javax.inject._
 
-import scala.collection.JavaConverters.asScalaBufferConverter
-
 @Singleton
 class ConfigProvider extends Provider[Config] {
 
@@ -13,8 +11,4 @@ class ConfigProvider extends Provider[Config] {
 
   override def get() = conf
 
-}
-
-object ConfigProvider {
-  def asScalaList[A](l: java.util.List[A]): Seq[A] = asScalaBufferConverter(l).asScala.toList
 }
